970dc188c3ea36714bc7206b23f0f4bbc8f48839
yusuf <yusuf.cakmak@ruutapp.com>
2024-02-15 12:46:19 +0700

    initial commit

---
+++ lokios/lokios/lokiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  lokiosApp.swift
+//  lokios
+//
+//  Created by yusuf-ruut on 15.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct lokiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
